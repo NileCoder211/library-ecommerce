efd79cc04d779b423487420c613ad100c88eaaa8
NileCoder211 <nilecoder8@gmail.com>
2026-04-09 02:35:41 +0300

    Refactor static file serving for production

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,10 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
 });
 }
 
