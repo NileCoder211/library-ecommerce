fb172bd313d5b24cc531c67a6107914540f41164
NileCoder211 <nilecoder8@gmail.com>
2026-05-25 03:01:17 +0300

    Change CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(passport.initialize());
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://library-ecommerce.onrender.com/",
   credentials: true
 }))
 
